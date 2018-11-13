43ced5fec939f72feb009a41190c7bc94ca1f293
BENAGP <cymfghj@me.com>
2018-11-13 17:44:15 +0800

    cmake

---
+++ S/build/CMakeFiles/S.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/S.dir/src/main.c.o"
+  "CMakeFiles/S.dir/src/S.c.o"
+  "S.pdb"
+  "S"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/S.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
